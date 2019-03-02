e5f55273ce474695c033bce4bc2fd8d8e5329c50
Atisaya Jain <atisayajain@gmail.com>
2019-03-02 14:36:49 +0530

    Initialised Firebase.

---
+++ app/src/main/java/com/atisayajain/agragamiexpert/MainActivity.java
@@ -0,0 +1,34 @@
+package com.atisayajain.agragamiexpert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    public FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ...
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Log.d("Logged in", currentUser.toString());
+        //updateUI(currentUser);
+    }
+
+}
